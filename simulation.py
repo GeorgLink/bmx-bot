b1130c0e3eef73e0d5be8c12d1411a00c5f5e4a6
Georg Link <linkgeorg@gmail.com>
2018-01-22 16:56:17 -0600

    a

--- simulation.py
+++ simulation.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+# imports
+from subprocess import call
+
+# base line: python executes
 print "Hello World"
+
+# access the bugmark exchange
 call(["bmx", "issue list"])
