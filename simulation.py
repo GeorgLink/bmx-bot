8d5d79f3693541163840b78ff1a7c4100e0d5dd1
andyl <andy@r210.com>
2018-01-22 16:15:49 -0800

    a

--- simulation.py
+++ simulation.py
@@ -2,10 +2,22 @@
 # -*- coding: UTF-8 -*-
 
 # imports
-from subprocess import call
+from subprocess import call, check_output
+import json
 
 # base line: python executes
 print "Hello World"
 
 # access the bugmark exchange
-call(["bmx", "issue list"])
+#call(["ls", "-l"])
+#raw_input("Press Enter to continue...")
+#call(["bmx", "issue", "list"])
+#call(["/home/deploy/.gem/ruby/2.5.0/bin/bmx", "issue", "list"])
+issue_list = check_output(["bmx","issue","list"])
+#print type(issue_list)
+# issue_list = issue_list.replace(":uuid", "uuid")
+print issue_list
+# raw_input("ENTER")
+#json_il = json.loads(issue_list)
+#json.dumps(json_il, separators=(',',':'))
+
