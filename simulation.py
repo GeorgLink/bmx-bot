886ecc0a57cc56dc689721facd33ea6a64217f68
andyl <andy@r210.com>
2018-01-22 18:40:31 -0800

    simple python script

--- simulation.py
+++ simulation.py
@@ -8,15 +8,9 @@ import json
 # base line: python executes
 # print "Hello World"
 
-# access the bugmark exchange
-#call(["ls", "-l"])
-#raw_input("Press Enter to continue...")
-#call(["bmx", "issue", "list"])
-#call(["/home/deploy/.gem/ruby/2.5.0/bin/bmx", "issue", "list"])
+# access the bugmark exchange and get list of issues
 issue_list = check_output(["bmx","issue","list"])
-#print type(issue_list)
-# issue_list = issue_list.replace(":uuid", "uuid")
-print issue_list
-# raw_input("ENTER")
-#json_il = json.loads(issue_list)
-#json.dumps(json_il, separators=(',',':'))
+# print the list of issues
+json_il = json.loads(issue_list)
+for p in json_il:
+    print p['uuid']
