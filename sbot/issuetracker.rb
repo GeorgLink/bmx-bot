9cdfffcfeef98438ab854b97038ebea22920e9d2
Georg J.P. Link <linkgeorg@gmail.com>
2018-04-08 09:29:15 -0500

    Add health metrics (#10)

--- sbot/issuetracker.rb
+++ sbot/issuetracker.rb
@@ -9,13 +9,15 @@ require File.expand_path("~/src/bugmark/config/environment")
 
 class Bmxsim_Issue
   def initialize(id, repo_uuid, project=1, difficulty=1)
-    @status = '   open'  # closed or open
+    @status = 'open'  # closed or open
     @progress = 0  # percentage of completion
     @project = project  # in the simulation we have different projects
     @difficulty = difficulty  # difficulty level of issue
     @id = id  # id of this issue
     @bmx_issue = FB.create(:issue, stm_repo_uuid: repo_uuid, exid: id, stm_status: "open").issue
     @uuid = @bmx_issue.uuid
+    @create_day = $sim_day
+    @close_day = nil
   end
   def uuid
     @uuid
@@ -23,10 +25,12 @@ class Bmxsim_Issue
   def close
     @status = 'closed'
     IssueCmd::Sync.new({exid: @id, stm_status: "closed"}).project
+    @close_day = $sim_day
   end
   def reopen
-    @status = '   open '
+    @status = 'open'
     IssueCmd::Sync.new({exid: @id, stm_status: "open"}).project
+    @close_day = nil
   end
   def get_status
     @status
@@ -36,6 +40,12 @@ class Bmxsim_Issue
   end
   def get_id
     @id
+  end
+  def get_age
+    $sim_day - @create_day
+  end
+  def get_resolution_days
+    @close_day - @create_day
   end
   def get_difficulty
     @difficulty
@@ -68,6 +78,60 @@ class Bmxsim_IssueTracker
   end
   def get_project_repo_uuid(proj_number)
     @project_bmx_repo_uuid.fetch(proj_number)
+  end
+  def get_project_health(proj_number)
+    repo_uuid = get_project_repo_uuid(proj_number)
+    proj_health = {
+      open_issues: nil,  # What is the number of open issues?
+      closed_issues: nil,  # What is the number of closed issues?
+      resolution_efficiency: nil, # What is the number of closed issues/number of abandoned issues?
+      open_issue_age: nil,  # What is the the age of open issues?
+      closed_issue_resolution_duration: nil  # What is the duration of time for issues to be resolved?
+    }
+
+    # Open Issues --> What is the number of open issues?
+    proj_health[:open_issues] = Issue.where(stm_repo_uuid: "#{repo_uuid}").open.count
+
+    # Closed Issues --> What is the number of closed issues?
+    proj_health[:closed_issues] = Issue.where(stm_repo_uuid: "#{repo_uuid}").closed.count
+
+    # Issue Resolution Efficiency --> What is the number of closed issues/number of abandoned issues?
+    # abandoned issue = open issue with no no offer and no open contract
+
+    # issues with open contracts
+    issues_not_abandoned = []
+    issues_contracted = Contract.joins(:issue).open.where('issues.stm_status = ?', 'open').pluck('stm_issue_uuid')
+    issues_not_abandoned.concat(issues_contracted)
+    issues_offered = Offer.joins(:issue).open.where('issues.stm_status = ?', 'open').pluck('stm_issue_uuid')
+    issues_not_abandoned.concat(issues_offered)
+    abandoned_issue_count = Issue.open.where('uuid NOT IN (?)', issues_not_abandoned).count
+
+
+    proj_health[:resolution_efficiency] = proj_health[:closed_issues].to_f / abandoned_issue_count.to_f
+
+    # Open Issue Age --> What is the the age of open issues?
+    ages = 0
+    issues = 0
+    @issues.each do |iss|
+      if iss.get_status == 'open'
+        ages += iss.get_age
+        issues += 1
+      end
+    end
+    proj_health[:open_issue_age] = ages.to_f/issues.to_f
+
+    # Closed Issue Resolution Duration --> What is the duration of time for issues to be resolved?
+    ages = 0
+    issues = 0
+    @issues.each do |iss|
+      if iss.get_status == 'closed'
+        ages += iss.get_resolution_days
+        issues += 1
+      end
+    end
+    proj_health[:closed_issue_resolution_duration] = ages.to_f/issues.to_f
+
+    return proj_health
   end
   def open_issue(project=1, difficulty=0)
     # puts "new issue #{(@issues.count+1)}"
--- sbot/run.rb
+++ sbot/run.rb
@@ -5,140 +5,152 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-PROCNAME = "cscw_bot"
-Process.setproctitle(PROCNAME)
+require 'benchmark'
+time = Benchmark.measure do
+  PROCNAME = "cscw_bot"
+  Process.setproctitle(PROCNAME)
 
-# for C-c
-trap "SIGINT" do
-  puts "\nExiting"
-  exit 130
-end
+  # for C-c
+  trap "SIGINT" do
+    puts "\nExiting"
+    exit 130
+  end
 
-# for "pkill -f fast_bot_buy"
-trap "SIGTERM" do
-  puts "TERMINATED #{Time.now}"
+  # for "pkill -f fast_bot_buy"
+  trap "SIGTERM" do
+    puts "TERMINATED #{Time.now}"
+    STDOUT.flush
+    exit 130
+  end
+
+  # SIMULATION PARAMETERS
+  NUMBER_OF_WORKERS = 1
+  NUMBER_OF_FUNDERS = 1  # equals number of projects
+  NUMBER_OF_ISSUES_DAILY_PER_FUNDER = 10  # equals number of offers created; #issue=#offer
+  MATURATION_DAYS_IN_FUTURE = 5 # end of:  0 = today, 1 = tomorrow
+  FUNDER_STARTING_BALANCE = 100000000
+  WORKER_STARTING_BALANCE = 0
+  WORKER_SKILLS = [1]  # ability to randomly create workers with different skills
+  RUN_SIMULATION_DAYS = 60
+
+  # output
+  BMXSIM_OUTPUT = 1  # 0 no output, 1 slim output, 9 detailed output
+
+  # run in turbo mode
+  BMX_SAVE_EVENTS  = "FALSE"
+  BMX_SAVE_METRICS = "FALSE"
+
+  # global day variable
+  $sim_day = 0
+
+  require 'io/console'
+  def continue_story
+    print "press any key"
+    STDIN.getch
+    print "            \r" # extra space to overwrite in case next sentence is short
+  end
+
+
+  puts "----- BUGMARK BOT -------------------------------------------"
+  puts "START #{Time.now} | C-c to exit"
+  puts "Process Name: #{PROCNAME}" if BMXSIM_OUTPUT > 0
+  puts "Loading Environment..." if BMXSIM_OUTPUT > 0
   STDOUT.flush
-  exit 130
-end
 
-# SIMULATION PARAMETERS
-NUMBER_OF_WORKERS = 10
-NUMBER_OF_FUNDERS = 4  # equals number of projects
-NUMBER_OF_ISSUES_DAILY_PER_FUNDER = 3  # equals number of offers created; #issue=#offer
-MATURATION_DAYS_IN_FUTURE = 7 # end of:  0 = today, 1 = tomorrow
-FUNDER_STARTING_BALANCE = 100000000
-WORKER_STARTING_BALANCE = 0
-WORKER_SKILLS = [1]  # ability to randomly create workers with different skills
-RUN_SIMULATION_DAYS = 30
+  require File.expand_path("~/src/bugmark/config/environment")
 
-# run in turbo mode
-BMX_SAVE_EVENTS  = "FALSE"
-BMX_SAVE_METRICS = "FALSE"
+  # delete all host data and create admin user
+  BugmHost.reset
+  BugmTime.set_day_offset(-1 * RUN_SIMULATION_DAYS)
 
-require 'io/console'
-def continue_story
-  print "press any key"
-  STDIN.getch
-  print "            \r" # extra space to overwrite in case next sentence is short
-end
+  puts "Simulate #{RUN_SIMULATION_DAYS}, starting on #{BugmTime.now}"
 
-puts "----- BUGMARK BOT -------------------------------------------"
-puts "START #{Time.now} | C-c to exit"
-puts "Process Name: #{PROCNAME}"
-puts "Loading Environment..."
-STDOUT.flush
+  # simulation classes
+  require_relative 'issuetracker'
+  require_relative 'person'
 
-require File.expand_path("~/src/bugmark/config/environment")
+  # create Issue Tracker
+  issue_tracker = Bmxsim_IssueTracker.new
 
-# delete all host data and create admin user
-BugmHost.reset
-BugmTime.set_day_offset(-1 * RUN_SIMULATION_DAYS)
-
-puts "Simulate #{RUN_SIMULATION_DAYS}, starting on #{BugmTime.now}"
-
-puts "Simulate #{SIMULATION_DAYS}, starting on #{BugmTime.now}"
-
-# simulation classes
-require_relative 'issuetracker'
-require_relative 'person'
-
-# create Issue Tracker
-issue_tracker = Bmxsim_IssueTracker.new
-
-# create funders and workers
-funders = []
-(1..NUMBER_OF_FUNDERS).to_a.each do |funder_id|
-  STDOUT.write "\rcreate funders: #{funder_id} / #{NUMBER_OF_FUNDERS}"
-  funder = FB.create(:user, email: "funder#{funder_id}@bugmark.net", balance: FUNDER_STARTING_BALANCE).user
-  funders.push(Bmxsim_Funder_FixedPay.new(funder, issue_tracker, funder_id))
-  # case funder_id
-  # when 1
-  #   funders.push(Bmxsim_Funder_InversePay.new(funder, issue_tracker))
-  # when 2
-  #   funders.push(Bmxsim_Funder_CorrelatedPay.new(funder, issue_tracker))
-  # when 3
-  #   funders.push(Bmxsim_Funder_FixedPay.new(funder, issue_tracker))
-  # else
-  #   funders.push(Bmxsim_Funder_RandomPay.new(funder, issue_tracker))
-  # end
-end
-puts ""
-workers = []
-(1..NUMBER_OF_WORKERS).to_a.each do |worker_id|
-  STDOUT.write "\rcreate workers: #{worker_id} / #{NUMBER_OF_WORKERS}"
-  worker = FB.create(:user, email: "worker#{worker_id}@bugmark.net", balance: WORKER_STARTING_BALANCE).user
-  # skill = (1..3).to_a.sample
-  skill = WORKER_SKILLS.sample
-  workers.push(Bmxsim_Worker_Treatment_NoMetricsWithPrices.new(worker, issue_tracker, skill, "w#{worker_id}"))
-  # group_size = NUMBER_OF_WORKERS/4
-  # case worker_id
-  # when (1..group_size)
-  #   workers.push(Bmxsim_Worker_Treatment_NoMetrics.new(worker, issue_tracker, skill))
-  # when ((group_size+1)..(2*group_size))
-  #   workers.push(Bmxsim_Worker_Treatment_HealthMetrics.new(worker, issue_tracker, skill))
-  # when ((2*group_size+1)..(3*group_size))
-  #   workers.push(Bmxsim_Worker_Treatment_MarketMetrics.new(worker, issue_tracker, skill))
-  # else
-  #   workers.push(Bmxsim_Worker_Treatment_BothMetrics.new(worker, issue_tracker, skill))
-  # end
-end
-puts ""
-
-# loop for each day
-(1..RUN_SIMULATION_DAYS).to_a.each do |day|
-  puts "Day #{day}: #{BugmTime.now}"
-  # print "#{day}: "
-  # call funders in a random order
-  funders.shuffle.each do |funder|
-    # print "f"
-    funder.do_work
+  # create funders and workers
+  funders = []
+  (1..NUMBER_OF_FUNDERS).to_a.each do |funder_id|
+    STDOUT.write "\rcreate funders: #{funder_id} / #{NUMBER_OF_FUNDERS}"  if BMXSIM_OUTPUT > 0
+    funder = FB.create(:user, email: "funder#{funder_id}@bugmark.net", balance: FUNDER_STARTING_BALANCE).user
+    funders.push(Bmxsim_Funder_FixedPay.new(funder, issue_tracker, funder_id))
+    # case funder_id
+    # when 1
+    #   funders.push(Bmxsim_Funder_InversePay.new(funder, issue_tracker))
+    # when 2
+    #   funders.push(Bmxsim_Funder_CorrelatedPay.new(funder, issue_tracker))
+    # when 3
+    #   funders.push(Bmxsim_Funder_FixedPay.new(funder, issue_tracker))
+    # else
+    #   funders.push(Bmxsim_Funder_RandomPay.new(funder, issue_tracker))
+    # end
   end
-  # call workers in a random order
-  workers.shuffle.each do |worker|
-    # print "w"
-    worker.do_work
-    puts "worker[#{worker.get_name}:#{worker.get_skill}](#{worker.get_balance}): #{worker.issue_status}"
+  puts "" if BMXSIM_OUTPUT > 0
+  workers = []
+  (1..NUMBER_OF_WORKERS).to_a.each do |worker_id|
+    STDOUT.write "\rcreate workers: #{worker_id} / #{NUMBER_OF_WORKERS}" if BMXSIM_OUTPUT > 0
+    worker = FB.create(:user, email: "worker#{worker_id}@bugmark.net", balance: WORKER_STARTING_BALANCE).user
+    # skill = (1..3).to_a.sample
+    skill = WORKER_SKILLS.sample
+    workers.push(Bmxsim_Worker_Treatment_NoMetricsWithPrices.new(worker, issue_tracker, skill, "w#{worker_id}"))
+    # group_size = NUMBER_OF_WORKERS/4
+    # case worker_id
+    # when (1..group_size)
+    #   workers.push(Bmxsim_Worker_Treatment_NoMetrics.new(worker, issue_tracker, skill))
+    # when ((group_size+1)..(2*group_size))
+    #   workers.push(Bmxsim_Worker_Treatment_HealthMetrics.new(worker, issue_tracker, skill))
+    # when ((2*group_size+1)..(3*group_size))
+    #   workers.push(Bmxsim_Worker_Treatment_MarketMetrics.new(worker, issue_tracker, skill))
+    # else
+    #   workers.push(Bmxsim_Worker_Treatment_BothMetrics.new(worker, issue_tracker, skill))
+    # end
   end
-  # go to next day
-  puts " next day"
-  BugmTime.go_past_end_of_day
-  # resolve contracts
-  STDOUT.write "resolve contracts: 0 / 0"
-  counter = 0
-  max_counter = Contract.pending_resolution.count
-  Contract.pending_resolution.each do |contract|
-    counter += 1
-    STDOUT.write "\rresolve contracts: #{counter} / #{max_counter}"
-    ContractCmd::Resolve.new(contract).project
-  end
-  #signal end of day
-  puts " DAY COMPLETE" ; STDOUT.flush
-  # continue_story  # wait for key press
-end
+  puts "" if BMXSIM_OUTPUT > 0
 
-puts "--------------------------- simulation finished ---------------------------"
-# Calling binding.pry to allow investigating the state of the simulation
-# Type "c" to continue and end the program
-binding.pry
-puts 'FINI'
+  # loop for each day
+  (1..RUN_SIMULATION_DAYS).to_a.each do |day|
+    puts "Day #{day}: #{BugmTime.now}"  if BMXSIM_OUTPUT > 0
+    $sim_day = day
+    # call funders in a random order
+    funders.shuffle.each do |funder|
+      print "f" if BMXSIM_OUTPUT > 0 && BMXSIM_OUTPUT < 9
+      funder.do_work
+      puts "funder[#{funder.get_name}](#{funder.get_balance})"  if BMXSIM_OUTPUT > 8
+    end
+    # call workers in a random order
+    workers.shuffle.each do |worker|
+      print "w"  if BMXSIM_OUTPUT > 0 && BMXSIM_OUTPUT < 9
+      worker.do_work
+      puts "worker[#{worker.get_name}:#{worker.get_skill}](#{worker.get_balance}): #{worker.issue_status}" if BMXSIM_OUTPUT > 8
+    end
+    # go to next day
+    puts " next day"  if BMXSIM_OUTPUT > 0
+    BugmTime.go_past_end_of_day
+    # resolve contracts
+    STDOUT.write "resolve contracts: 0 / 0" if BMXSIM_OUTPUT > 0
+    counter = 0
+    max_counter = Contract.pending_resolution.count
+    Contract.pending_resolution.each do |contract|
+      counter += 1
+      STDOUT.write "\rresolve contracts: #{counter} / #{max_counter}" if BMXSIM_OUTPUT > 0
+      ContractCmd::Resolve.new(contract).project
+    end
+    #signal end of day
+    puts " DAY COMPLETE"  if BMXSIM_OUTPUT > 0
+    STDOUT.flush
+    # continue_story  # wait for key press
+  end
+
+  # Calling binding.pry to allow investigating the state of the simulation
+  # Type "c" to continue and end the program
+  issue_tracker.get_project_health(1)
+  binding.pry
+  puts "--------------------------- simulation finished ---------------------------"
+end
+puts time
+puts 'FINI' if false  # this line is only here for binding.pry to work
 # FINI
