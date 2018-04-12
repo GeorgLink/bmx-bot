4d8b369c19b295e71175d4967d1591dccc3583dc
Georg Link <linkgeorg@gmail.com>
2018-04-11 20:59:12 -0500

    cscw-bot

--- sbot/run.rb
+++ sbot/run.rb
@@ -25,14 +25,14 @@ time = Benchmark.measure do
   end
 
   # SIMULATION PARAMETERS
-  NUMBER_OF_WORKERS = 2
+  NUMBER_OF_WORKERS = 1
   NUMBER_OF_FUNDERS = 1  # equals number of projects
-  NUMBER_OF_ISSUES_DAILY_PER_FUNDER = 3  # equals number of offers created; #issue=#offer
-  MATURATION_DAYS_IN_FUTURE = 5 # end of:  0 = today, 1 = tomorrow
+  NUMBER_OF_ISSUES_DAILY_PER_FUNDER = 1  # equals number of offers created; #issue=#offer
+  MATURATION_DAYS_IN_FUTURE = 0 # end of:  0 = today, 1 = tomorrow
   FUNDER_STARTING_BALANCE = 100000000
   WORKER_STARTING_BALANCE = 0
   WORKER_SKILLS = [1]  # ability to randomly create workers with different skills
-  RUN_SIMULATION_DAYS = 5
+  RUN_SIMULATION_DAYS = 2
 
   # output
   BMXSIM_OUTPUT = 1  # 0 no output, 1 slim output, 9 detailed output
